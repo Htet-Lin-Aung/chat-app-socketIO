59a6a8a0bcc873c898e38014b039e47a24800bf0
Htet Lin Aung <htetlinaungtuinfo@gmail.com>
2023-11-04 22:34:30 +0630

    updated: handle signIn

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -14,11 +14,22 @@ import { styles } from "../utils/styles";
 const Login = ({ navigation }) => {
     const [username, setUsername] = useState("");
 
+    const storeUsername = async () => {
+        try {
+            //👇🏻 async function - saves the username to AsyncStorage
+            //   redirecting to the Chat page
+            await AsyncStorage.setItem("username", username);
+            navigation.navigate("Chat");
+        } catch (e) {
+            Alert.alert("Error! While saving username");
+        }
+    };
+
     //👇🏻 checks if the input field is empty
     const handleSignIn = () => {
         if (username.trim()) {
-            //👇🏻 Logs the username to the console
-            console.log({ username });
+            //👇🏻 calls AsyncStorage function
+            storeUsername();
         } else {
             Alert.alert("Username is required.");
         }
