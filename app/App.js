46c973624492932cc63fc6ddef1087f65927947a
Htet Lin Aung <htetlinaungtuinfo@gmail.com>
2023-11-11 21:29:18 +0630

    updated: navigate and login

--- app/App.js
+++ app/App.js
@@ -2,6 +2,8 @@ import React from "react";
 
 //👇🏻 app screens
 import Login from "./screens/Login";
+import Messaging from "./screens/Messaging";
+import Chat from "./screens/Chat";
 
 //👇🏻 React Navigation configurations
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,15 +12,25 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name='Login'
-                    component={Login}
-                    options={{ headerShown: false }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name='Login'
+					component={Login}
+					options={{ headerShown: false }}
+				/>
+
+				<Stack.Screen
+					name='Chat'
+					component={Chat}
+					options={{
+						title: "Chats",
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen name='Messaging' component={Messaging} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
